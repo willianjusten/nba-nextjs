46d1d0b417f9a1a43c23cb4ee00202b21341e079
Willian Justen <willianjustenqui@gmail.com>
2024-01-29 01:40:14 -0300

    Prevent caching

--- src/app/api/today/route.ts
+++ src/app/api/today/route.ts
@@ -1,9 +1,10 @@
 import { parseGames } from "@/helpers/mappers";
 
+export const revalidate = 0;
+
 export async function GET() {
   const res = await fetch(
-    `https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json`,
-    { next: { revalidate: 0 } }
+    `https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json`
   );
 
   const data = await res.json();
