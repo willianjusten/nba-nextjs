904594e56bf02cd93c3cfcb488797e95cf9d1299
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2024-03-02 20:48:59 -0300

    bugfix: update to display record even on playoffs or zero

--- app/components/TeamInfo/index.tsx
+++ app/components/TeamInfo/index.tsx
@@ -15,12 +15,13 @@ function TeamInfo({ team }: TeamInfoProps) {
   const logo = !!team.teamId
     ? `https://cdn.nba.com/logos/nba/${team.teamId}/primary/L/logo.svg`
     : "https://cdn.nba.com/logos/nba/fallback.svg";
+  const hasRecord = team.wins !== undefined && team.losses !== undefined;
 
   return (
     <div className="flex w-1/4 flex-col items-center text-center">
       <Image src={logo} width={48} height={48} alt={team.teamName} />
       <p className="mt-1 whitespace-nowrap text-sm font-semibold">{teamName}</p>
-      {!!team.wins && !!team.losses && (
+      {hasRecord && (
         <p className="text-xs text-gray-400">{`${team.wins}-${team.losses}`}</p>
       )}
     </div>
