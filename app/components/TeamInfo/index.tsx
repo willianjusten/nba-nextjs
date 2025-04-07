ae745ec7137709725d61663395a04060edc17007
André Moraes <andre.moraes.98@gmail.com>
2025-04-07 19:49:40 -0300

    feat: improve logo legibility

--- app/components/TeamInfo/index.tsx
+++ app/components/TeamInfo/index.tsx
@@ -19,7 +19,13 @@ function TeamInfo({ team }: TeamInfoProps) {
 
   return (
     <div className="flex w-1/4 flex-col items-center text-center">
-      <Image src={logo} width={48} height={48} alt={team.teamName} />
+      <Image
+        className="drop-shadow-[0_0_6px_rgba(255,255,255,0.6)]"
+        src={logo}
+        width={48}
+        height={48}
+        alt={team.teamName}
+      />
       <p className="mt-1 whitespace-nowrap text-sm font-semibold">{teamName}</p>
       {hasRecord && (
         <p className="text-xs text-gray-400">{`${team.wins}-${team.losses}`}</p>
