8e4cc8752150f907933238cbd85b90ac1ddd58e9
Bruna Grassi <brunagm88@gmail.com>
2026-03-26 14:42:26 -0300

    chore: replace SWR with tankStack

--- app/(pages)/game/[id]/page.tsx
+++ app/(pages)/game/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { format } from "date-fns/format";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
 
-import SWRProvider from "@/app/components/SWRProvider";
+import QueryClientProvider from "@/app/components/QueryClientProvider";
 import { GameDetails } from "@/app/components";
 import { DATE_TITLE_FORMAT } from "@/app/constants";
 
@@ -39,20 +40,21 @@ export async function generateMetadata(props: GameDetailsProps) {
 
 export default async function Page(props: GameDetailsProps) {
   const params = await props.params;
-
-  const {
-    id
-  } = params;
+  const { id } = params;
 
   const data = await getData(id);
+  const queryClient = new QueryClient();
 
-  const fallback = {
-    [`/api/game/${id}`]: data,
-  };
+  await queryClient.prefetchQuery({
+    queryKey: ["game", id],
+    queryFn: () => getData(id),
+  });
+
+  const dehydratedState = dehydrate(queryClient);
 
   return (
-    <SWRProvider fallback={fallback}>
+    <QueryClientProvider dehydratedState={dehydratedState}>
       <GameDetails id={id} />
-    </SWRProvider>
+    </QueryClientProvider>
   );
 }
--- app/components/Games/index.tsx
+++ app/components/Games/index.tsx
@@ -1,17 +1,23 @@
 "use client";
 
-import useSWR from "swr";
+import { useQuery } from "@tanstack/react-query";
 
 import { DateSelector, GamesList } from "@/app/components";
 import { getDays, ParsedGames } from "@/app/helpers";
 
-const fetcher = (...args: [RequestInfo, RequestInit]) =>
-  fetch(...args).then((res) => res.json());
+const fetcher = async (): Promise<ParsedGames> => {
+  const res = await fetch("/api/today");
+  return res.json();
+};
 
 function Games() {
-  const { data } = useSWR<ParsedGames>("/api/today", fetcher, {
-    refreshInterval: 15000,
+  const { data } = useQuery<ParsedGames>({
+    queryKey: ["today"],
+    queryFn: fetcher,
+    refetchInterval: 15000,
+    staleTime: 15000,
   });
+
   const { day, prevDay, nextDay } = getDays(data?.gameDate);
 
   return (
