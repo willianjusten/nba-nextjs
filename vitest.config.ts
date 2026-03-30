6514b4b5f6b4e366937de2cfd1e6b7970c8e898e
Bruna Grassi <brunagm88@gmail.com>
2026-03-30 11:53:52 -0300

    chore: replace jest-dom with happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,8 +6,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: ["./vitest.setup.ts"],
+    environment: "happy-dom",
     include: ["**/*.{test,spec}.{ts,tsx}", "**/test.{ts,tsx}"],
     coverage: {
       reporter: ["text", "json", "html"],
