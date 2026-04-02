1d606a1ae6121de1354e34b0ed5e2d80798e31f3
Bruna Grassi <brunagm88@gmail.com>
2026-04-02 13:01:52 -0300

    chore: trigger progress bar on back navigation

--- app/components/BackButton/index.tsx
+++ app/components/BackButton/index.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { ArrowIcon } from "@/app/components";
+import { useRouter } from "@bprogress/next/app";
 
 function BackButton() {
+  const router = useRouter();
+
   return (
     <div
       className="flex max-w-fit items-center py-5 transition-all duration-200 hover:cursor-pointer hover:opacity-60"
-      onClick={() => window.history.back()}
+      onClick={() => {
+        router.back();
+      }}
     >
       <ArrowIcon size={16} />
       <span className="pl-3 text-xl">Back</span>
