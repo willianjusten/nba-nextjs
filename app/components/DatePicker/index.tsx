6bfdf38781e4e7d87a149923ac7f75096406215c
Bruna Grassi <brunagm88@gmail.com>
2026-04-02 12:55:59 -0300

    fix: replace import to fix progress bar not showing when changing date

--- app/components/DatePicker/index.tsx
+++ app/components/DatePicker/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "@bprogress/next/app";
 import { format, parseISO } from "date-fns";
 import {
   Calendar,
